ec35421781ecdec90fd986dffa2e231575cb8e8f
Max Carlson <max.carlson@brainium.com>
2024-11-15 05:37:06 -0800

    rsync android to windows

--- rsyncAndroidToSlice.sh
+++ rsyncAndroidToSlice.sh
@@ -4,15 +4,29 @@
 SOURCE_FOLDER="./"
 SSH_USER="mcarls"
 BASE_IP="192.168.50"
-LAST_OCTET="105"
+LAST_OCTET="100"
 DELETE_SOURCE="false"
 DEST_FOLDER="~/"
+CONVERT_TO_WSL_PATH="true"
+SSH_PORT="2222"
 
 # Function to print usage
 usage() {
-    echo "Usage: $0 [-u user] [-D] -p remote_path [source_folder] [base_ip] [last_octet]"
-    echo "Example: $0 -u mcarls -D -p /path/on/remote/machine /path/to/source_folder 192.168.1 105"
+    echo "Usage: $0 [-u user] [-D] -p remote_path [source_folder] [base_ip] [last_octet] [-P port] [--no-wsl-path]"
+    echo "Example: $0 -u mcarls -D -p C:/Users/mcarls/Downloads ~/storage/downloads/Seal/ 192.168.1 105 -P 2222"
     exit 1
+}
+
+# Function to convert Windows path to WSL path
+convert_to_wsl_path() {
+    local path="$1"
+    if [[ "$path" =~ ^([a-zA-Z]):[\\/](.*) ]]; then
+        local drive="${BASH_REMATCH[1],,}" # Convert drive letter to lowercase
+        local subpath="${BASH_REMATCH[2]//\\//}" # Replace backslashes with slashes
+        echo "/mnt/${drive}/${subpath}"
+    else
+        echo "$path" # Return the original path if it doesn't match Windows-style
+    fi
 }
 
 # Parse options
@@ -21,8 +35,10 @@ while [[ "$#" -gt 0 ]]; do
         -u|--user) SSH_USER="$2"; shift ;;
         -D|--delete-source) DELETE_SOURCE="true" ;;
         -p|--path) DEST_FOLDER="$2"; shift ;;
+        -P|--port) SSH_PORT="$2"; shift ;;
+        --no-wsl-path) CONVERT_TO_WSL_PATH="false" ;;
         -h|--help) usage ;;
-        *) 
+        *)
             if [ -z "$SOURCE_FOLDER_PROVIDED" ]; then
                 SOURCE_FOLDER="$1"
                 SOURCE_FOLDER_PROVIDED="true"
@@ -47,11 +63,20 @@ if [ -z "$DEST_FOLDER" ]; then
     usage
 fi
 
+# Convert paths to WSL paths if the feature is enabled
+if [[ "$CONVERT_TO_WSL_PATH" == "true" ]]; then
+    DEST_FOLDER=$(convert_to_wsl_path "$DEST_FOLDER")
+    SOURCE_FOLDER=$(convert_to_wsl_path "$SOURCE_FOLDER")
+fi
+
 # Construct the full destination IP address
 DEST_IP="${BASE_IP}.${LAST_OCTET}"
 
+# Print the destination IP for debugging
+echo "Destination IP: $DEST_IP"
+
 # Rsync command with options
-RSYNC_CMD="rsync -avzP"
+RSYNC_CMD="rsync -av --progress --info=progress2 -e \"ssh -p $SSH_PORT\""
 
 # If delete-source option is enabled, add the remove-source-files flag
 if [[ "$DELETE_SOURCE" == "true" ]]; then
@@ -59,7 +84,7 @@ if [[ "$DELETE_SOURCE" == "true" ]]; then
 fi
 
 # Execute the rsync command
-$RSYNC_CMD "$SOURCE_FOLDER" "${SSH_USER}@${DEST_IP}:${DEST_FOLDER}"
+eval $RSYNC_CMD "$SOURCE_FOLDER" "${SSH_USER}@${DEST_IP}:${DEST_FOLDER}"
 
 # Exit with success message
 if [ $? -eq 0 ]; then
@@ -68,4 +93,3 @@ else
     echo "Rsync transfer failed."
     exit 1
 fi
-
