bbd69b4133c4c321ac8895954ff02dfcb36c5927
Max Carlson <max.carlson@brainium.com>
2025-08-01 04:33:10 -0700

    updates

--- modules/termdash/__init__.py
+++ modules/termdash/__init__.py
@@ -3,5 +3,5 @@ TermDash: A robust, thread-safe library for creating persistent, multi-line,
 in-place terminal dashboards with a co-existing scrolling log region.
 """
 from .dashboard import TermDash
-from .components import Line, Stat
+from .components import Line, Stat, AggregatedLine
 from .utils import format_bytes
