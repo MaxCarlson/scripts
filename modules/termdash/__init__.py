ca045fb68743cb55e44ed060155f643a6deda6a6
Max Carlson <max.carlson@brainium.com>
2025-09-10 01:17:20 -0700

    termdash

--- modules/termdash/__init__.py
+++ modules/termdash/__init__.py
@@ -5,12 +5,16 @@ in-place terminal dashboards with a co-existing scrolling log region.
 from .dashboard import TermDash
 from .components import Line, Stat, AggregatedLine
 from .utils import format_bytes, fmt_hms, bytes_to_mib, clip_ellipsis
+from .progress import ProgressBar
+from .simpleboard import SimpleBoard
 
 __all__ = [
     "TermDash",
     "Line",
     "Stat",
     "AggregatedLine",
+    "ProgressBar",
+    "SimpleBoard",
     "format_bytes",
     "fmt_hms",
     "bytes_to_mib",
