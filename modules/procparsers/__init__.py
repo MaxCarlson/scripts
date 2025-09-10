b5596d09e73f07efc5f34a5b6237d0dc0160cfdb
Max Carlson <max.carlson@brainium.com>
2025-09-10 07:44:38 -0700

    procparsers - ytdlp and aebndl parsing library code-extract from ytaedl

---
+++ modules/procparsers/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+procparsers: Parsers for stdout/stderr streams of external tools.
+
+Currently supported:
+- yt-dlp
+- aebn-dl
+"""
+from .yt_dlp import parse_line as parse_ytdlp_line  # re-export
+from .aebndl import parse_line as parse_aebndl_line
+from .utils import sanitize_line
+
+__all__ = [
+    "parse_ytdlp_line",
+    "parse_aebndl_line",
+    "sanitize_line",
+]
